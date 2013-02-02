82eb115874eccddf5c51f4cf0e6e900d890a6050
Andrea Crotti <andrea.crotti.0@gmail.com>
2013-02-02 23:36:13 +0000

    add one test at a time

--- code/show_cov.py
+++ code/show_cov.py
@@ -2,7 +2,7 @@ import unittest
 
 
 def smart_division(a, b):
-    """Run a smart division
+    """Run a 'smart' division
     """
     if b == 0:
         raise Exception("Can not divide by 0")
@@ -17,20 +17,21 @@ def smart_division(a, b):
 
 
 class TestSmartDivision(unittest.TestCase):
-    def test_division_by_0(self):
-        """Test that dividing by 0 raises an exception
-        """
-        with self.assertRaises(Exception):
-            smart_division(10, 0)
+    pass
+    # def test_division_by_0(self):
+    #     """Test that dividing by 0 raises an exception
+    #     """
+    #     with self.assertRaises(Exception):
+    #         smart_division(10, 0)
 
-    def test_float_division(self):
-        """Check that the float division returns a correct result (with approximation)
-        """
-        self.assertAlmostEqual(smart_division(2, 3), 0.66, places=1)
+    # def test_float_division(self):
+    #     """Check that the float division returns a correct result (with approximation)
+    #     """
+    #     self.assertAlmostEqual(smart_division(2, 3), 0.66, places=1)
 
-    def test_int_division(self):
-        self.assertEqual(smart_division(1, 1), 1)
-        self.assertEqual(smart_division(10, 2), 5)
+    # def test_int_division(self):
+    #     self.assertEqual(smart_division(1, 1), 1)
+    #     self.assertEqual(smart_division(10, 2), 5)
 
 
 if __name__ == '__main__':
